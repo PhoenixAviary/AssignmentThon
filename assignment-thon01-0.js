a21062c7cdd9339799386d702919244f745effe3
Phoenix Aviary <valerieannphoenix@gmail.com>
2017-07-18 22:14:01 +0000

    javascript car assignment

---
+++ assignment-thon01-0.js
@@ -0,0 +1,18 @@
+
+var sedan = {
+    totNumAvail:3,
+    canRent:"yes",
+    rentalPrice:"$20 per day",
+};
+
+var suv = {
+    totNumAvail:1,
+    canRent:"yes",
+    rentalPrice:"$30 per day",
+};
+
+var sports = {
+    totNumAvail:2,
+    canRent:"yes",
+    rentalPrice:"$40 per day",
+};
